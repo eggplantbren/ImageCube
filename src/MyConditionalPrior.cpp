03d5752a296f9b18e56e116e3bc1c28b8e9bc75a
Brendon J. Brewer <brendon.brewer@gmail.com>
2018-03-10 13:09:42 +1300

    Uniform priors for now

--- src/MyConditionalPrior.cpp
+++ src/MyConditionalPrior.cpp
@@ -1,4 +1,5 @@
 #include "MyConditionalPrior.h"
+#include "Data.h"
 #include "DNest4/code/DNest4.h"
 #include <cmath>
 
@@ -25,22 +26,32 @@ double MyConditionalPrior::perturb_hyperparameters(DNest4::RNG& rng)
 
 // vec = { xc, yc, fc, mass, width, q, theta, fwidth }
 double MyConditionalPrior::log_pdf(const std::vector<double>& vec) const
-{q
+{
     return 0.0;
 }
 
 void MyConditionalPrior::from_uniform(std::vector<double>& vec) const
 {
-    vec[0] = x_min + (x_max - x_min)*vec[0];
-    vec[1] = y_min + (y_max - y_min)*vec[1];
-    vec[2] = f_min + (f_max - f_min)*vec[2];
-
-
+    vec[0] = Data::x_min + (Data::x_max - Data::x_min)*vec[0];
+    vec[1] = Data::y_min + (Data::y_max - Data::y_min)*vec[1];
+    vec[2] = Data::f_min + (Data::f_max - Data::f_min)*vec[2];
+    vec[3] = 10.0*vec[3];
+    vec[4] = 10.0*vec[4];
+    vec[5] = M_PI*vec[5];
+    vec[6] = 10.0*vec[6];
+    vec[7] = 200.0*vec[7];
 }
 
 void MyConditionalPrior::to_uniform(std::vector<double>& vec) const
 {
-
+    vec[0] = (vec[0] - Data::x_min)/(Data::x_max - Data::x_min);
+    vec[1] = (vec[1] - Data::y_min)/(Data::y_max - Data::y_min);
+    vec[2] = (vec[2] - Data::f_min)/(Data::f_max - Data::f_min);
+    vec[3] = vec[3] / 10.0;
+    vec[4] = vec[4] / 10.0;
+    vec[5] = vec[5] / M_PI;
+    vec[6] = vec[6] / 10.0;
+    vec[7] = vec[7] / 200.0;
 }
 
 void MyConditionalPrior::print(std::ostream& out) const
